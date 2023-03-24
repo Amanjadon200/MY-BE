f0ad66dd1f264eedf1ec6453f1cdfa89812cd1ba
AmanJadon2023 <aman.jadon@geminisolutions.com>
2023-03-24 17:15:44 +0530

    changes in logIn api , returning username field as well when user login

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+// console.log(app)
+//app is a object of functions
 const { ObjectId } = require("mongodb")
 const { connectToDb, getDb } = require('./db.js')
 let db;
@@ -11,7 +13,6 @@ app.options("*", cors({ origin: 'http://localhost:3000', optionsSuccessStatus: 2
 app.use(cors({ origin: "http://localhost:3000", optionsSuccessStatus: 200 }));
 app.use(cors());
 connectToDb((err) => {
-
     app.listen('3001', () => {
         console.log('server is listening at 3001')
     })
@@ -38,20 +39,20 @@ app.post('/tickets', (req, res) => {
         return res.json({ "error": "can't add a document" })
     })
 })
-app.delete('/tickets', async(req, res) => {
+app.delete('/tickets', async (req, res) => {
     const ticketToBeDeleted = req.query.id;
-    let _id=''
-    await db.collection('ticketdata').find().forEach((ticket)=>{
-    if(parseInt(ticket.id)===parseInt(ticketToBeDeleted)){
-        _id=ticket._id
-        return;
-    }
+    let _id = ''
+    await db.collection('ticketdata').find().forEach((ticket) => {
+        if (parseInt(ticket.id) === parseInt(ticketToBeDeleted)) {
+            _id = ticket._id
+            return;
+        }
     })
-    const data=await db.collection('ticketdata').deleteOne({_id:new ObjectId(_id)})
-    if(data.acknowledged==true){
+    const data = await db.collection('ticketdata').deleteOne({ _id: new ObjectId(_id) })
+    if (data.acknowledged == true) {
         res.send('deleted')
     }
-    else{
+    else {
         res.send("can't delete a document")
     }
 })
@@ -75,7 +76,7 @@ app.post('/register', async (req, res) => {
             a = 2;
         }
     })
-    if(a===2){
+    if (a === 2) {
         res.send({ "error": "user already exists" })
     }
     if (a === 1) {
@@ -88,20 +89,38 @@ app.post('/register', async (req, res) => {
 })
 
 
-app.post('/logIn',async(req,res)=>{
-    const dataObject=req.body;
-    const email=dataObject.data.email
-    const password=dataObject.data.password;
-    const user= await db.collection('user').findOne({email:email})
-    if(user){
-        if(user.password===password){
-             res.status(200).send('user exist')
+app.post('/logIn', async (req, res) => {
+    const dataObject = req.body;
+    const email = dataObject.data.email
+    const password = dataObject.data.password;
+
+    const user = await db.collection('user').findOne({ email: email })
+    if (user) {
+        if (user.password === password) {
+            res.status(200).send({message:'user exist',name:user.name})
         }
-        else{
+        else {
             res.status(401).send('password is wrong')
         }
     }
-    else{
-        res.status(404).send({'error':'user does not exit'})
+    else {
+        res.status(404).send({ 'error': 'user does not exit' })
     }
 })
+
+
+const abc = function (a, b) {
+    return {
+        sum: function sum(a, b) {
+            return a + b
+        }
+    , 
+        subtraction: function subtraction(a, b) {
+            return a - b
+        }
+    }
+}
+// console.log(typeof abc())
+// const d=abc();
+// console.log(d,d.sum(2,3),d.subtraction(2,3))
+// console.log(d(1,2))
